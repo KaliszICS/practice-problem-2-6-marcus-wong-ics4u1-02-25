0da2749e7caa403f981a54122ac99ccabd2d0b17
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-04-09 13:46:06 +0000

    same assignment 2.6

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -11,64 +11,70 @@ public class PracticeProblem {
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-                result[1]++; 
+                result[1]++; // comparison
                 if (nums[j] > nums[j + 1]) {
                     double temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
-                    result[0] += 3; 
+                    result[0] += 3; // each swap counts as 3 operations
                     swapped = true;
                 }
             }
-            if (!swapped) break; 
+            if (!swapped) break;
         }
+
         return result;
     }
 
     public static int[] selectionSort(double[] nums) {
-        int[] result = new int[2]; 
+        int swaps = 0;
+        int comparisons = 0;
         int n = nums.length;
 
         for (int i = 0; i < n - 1; i++) {
             int min = i;
             for (int j = i + 1; j < n; j++) {
-                result[1]++; 
+                comparisons++; 
                 if (nums[j] < nums[min]) {
+                    swaps++;
                     min = j;
+                    
                 }
             }
-            if (min != i) { 
+                swaps += 3;
                 double temp = nums[i];
                 nums[i] = nums[min];
                 nums[min] = temp;
-                result[0] += 5; 
+                
             }
+            return new int[]{swaps, comparisons};
         }
-        return result;
-    }
 
-	public static int[] insertionSort(double[] nums) {
-		int[] result = new int[2];
-		int n = nums.length;
-	
-		for (int i = 1; i < n; i++) {
-			double key = nums[i];
-			int index = i - 1;
-			while (index >= 0) {
-				result[1]++;
-				if (nums[index] > key) {
-					nums[index + 1] = nums[index];
-					result[0] += 2; 
-					index--;
-				} else {
-					break;
-				}
-			}
-			nums[index + 1] = key; 
-			result[0] += 1; 
-		}
-		return result;
-	}
+        
+
+    public static int[] insertionSort(double[] nums) {
+        int swaps = 0;
+        int comparisons = 0;
+        int n = nums.length;
+
+        for (int i = 1; i < n; i++) {
+            swaps++;
+            double key = nums[i];
+            int index = i - 1;
+
+            while (index >= 0 && nums[index] > key) {
+                comparisons++; 
+                nums[index + 1] = nums[index];
+                swaps++;
+                index--;
+            }
+
+            nums[index + 1] = key;
+            swaps++; // placing key
+        }
+
+        return new int[]{swaps, comparisons};
+    }
 
     public static String leastSwaps(double[] nums) {
         double[] copy1 = nums.clone();
@@ -79,16 +85,20 @@ public class PracticeProblem {
         int[] selectionResult = selectionSort(copy2);
         int[] insertionResult = insertionSort(copy3);
 
-        if (bubbleResult[0] <= selectionResult[0] && 
-            bubbleResult[0] <= insertionResult[0]) {
+        int bubbleSortSwap = bubbleResult[0];
+        int selectionSortSwap = selectionResult[0];
+        int insertionSortSwap = insertionResult[0];
+
+        if (bubbleSortSwap <= selectionSortSwap && bubbleSortSwap <= insertionSortSwap) {
             return "Bubble";
-        } else if (selectionResult[0] <= insertionResult[0]) {
+        }
+        else if (selectionSortSwap <= bubbleSortSwap && selectionSortSwap <= insertionSortSwap) {
             return "Selection";
-        } else {
+        }        
+        else {
             return "Insertion";
         }
     }
-
     public static String leastIterations(double[] nums) {
         double[] copy1 = nums.clone();
         double[] copy2 = nums.clone();
@@ -98,14 +108,19 @@ public class PracticeProblem {
         int[] selectionResult = selectionSort(copy2);
         int[] insertionResult = insertionSort(copy3);
 
-        if (bubbleResult[1] <= selectionResult[1] && 
-            bubbleResult[1] <= insertionResult[1]) {
+        int bubbleSortSwap = bubbleResult[1];
+        int selectionSortSwap = selectionResult[1];
+        int insertionSortSwap = insertionResult[1];
+
+        if (bubbleSortSwap <= selectionSortSwap && bubbleSortSwap <= insertionSortSwap) {
             return "Bubble";
-        } else if (selectionResult[1] <= insertionResult[1]) {
-            return "Selection";
-        } else {
-            return "Insertion";
         }
+        else if (selectionSortSwap <= bubbleSortSwap && selectionSortSwap <= insertionSortSwap) {
+            return "Selection";
+        }        
+        else {
+            return "Insertion";
     }
 
 	}
+}
